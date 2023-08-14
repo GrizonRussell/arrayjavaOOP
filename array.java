58df89aff1e2f13464e9cb321d7cedf8b2738147
gen.java <132185718+GrizonRussell@users.noreply.github.com>
2023-08-14 20:15:08 +0800

    added.array.java

---
+++ array.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class array{
+    public static void main(String[] args) {
+        // String[] name = {"mel", "griz", "francis", "ralph"};
+        // for (String i : name) {
+        // System.out.println(i);
+        // }
+        char[] JavaCharArray = {"griz", "mel", "francis", "ralph"};
+        for(char c:JavaCharArray){
+            System.out.println(c);
+        }
+    }
+}
